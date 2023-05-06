db8de84781e35c9248c60799f02386a5e69785f7
lxc <lxc@163.com>
2023-05-06 09:48:03 +0800

    lxc master first modify!

---
+++ src/main/java/com/zpark/ssm/App.java
@@ -0,0 +1,13 @@
+package com.zpark.ssm;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+
+        System.out.println("Hello Git! zza master second commit!");
+        System.out.println("Hello Git! zza hotfix first commit!");
+        System.out.println("Hello Git! lxc hotfix first modify!");
+        System.out.println("Hello Git! lxc master first modify!");
+    }
+}
